5740f2240aa017db00814d367493090db096d046
Ryo Tsunoda <try0.development@gmail.com>
2021-07-04 14:45:38 +0900

    公開パッケージテストプロジェクト追加

---
+++ tests/StampImages.Test.PublishedPackage/StampFactoryExamples.cs
@@ -0,0 +1,102 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StampImages.Core;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace StampImages.Test.NetFramework
+{
+    [TestClass]
+    public class StampFactoryExamples
+    {
+
+        StampImageFactory stampImageFactory = new StampImageFactory(new Core.StampImageFactoryConfig());
+
+        ~StampFactoryExamples()
+        {
+            stampImageFactory.Dispose();
+        }
+
+        /// <summary>
+        /// 日付スタンプ出力
+        /// </summary>
+        [TestMethod]
+        public void ExampleCreateThreeAreaCircularStamp()
+        {
+            var stamp = new ThreeAreaCircularStamp
+            {
+                TopText = new StampText { Value = "所属部門", Size = 22 },
+                MiddleText = new StampText { Value = DateTime.Now.ToString("yyyy.MM.dd"), Size = 30 },
+                BottomText = new StampText { Value = "ユーザー名", Size = 25 }
+            };
+
+            using (stamp)
+            {
+                stampImageFactory.Save(stamp, "./inkan_256.png");
+            }
+        }
+
+        /// <summary>
+        /// 四角形スタンプ出力
+        /// </summary>
+        [TestMethod]
+        public void ExampleCreateSquareStamp()
+        {
+            var stamp = new SquareStamp
+            {
+                EdgeType = StampEdgeType.DOUBLE,
+                TextOrientationType = TextOrientationType.VERTICAL,
+                Text = new StampText { Value = "承認", Size = 60 },
+            };
+            stamp.EffectTypes.Add(StampEffectType.NOISE);
+
+            using (stamp)
+            using (var bitmap = stampImageFactory.Create(stamp))
+            {
+                bitmap.Save("./inkan_sq_256.png");
+            }
+        }
+
+        /// <summary>
+        /// 円形スタンプ出力
+        /// </summary>
+        [TestMethod]
+        public void ExampleCreateCircularStamp()
+        {
+            var stamp = new CircularStamp
+            {
+                Text = new StampText { Value = "承認", Size = 60 },
+            };
+
+            using (stamp)
+            using (var bitmap = stampImageFactory.Create(stamp))
+            {
+                bitmap.Save("./inkan_circular_256.png");
+            }
+        }
+
+
+
+        /// <summary>
+        /// リサイズ
+        /// </summary>
+        [TestMethod]
+        public void ExampleResizeStamp()
+        {
+            var stamp = new ThreeAreaCircularStamp
+            {
+                TopText = new StampText { Value = "所属部門", Size = 22 },
+                MiddleText = new StampText { Value = DateTime.Now.ToString("yyyy.MM.dd"), Size = 30 },
+                BottomText = new StampText { Value = "ユーザー名", Size = 25 }
+            };
+
+            using (stamp)
+            using (Bitmap stampImage = stampImageFactory.Create(stamp))
+            using (Bitmap resized = stampImageFactory.Resize(stampImage, 128, 128))
+            {
+                resized.Save("./inkan_128.png", ImageFormat.Png);
+            }
+
+        }
+    }
+}
