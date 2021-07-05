5a41ed857a385a18dc6782b1d3103269945db03d
Ryo Tsunoda <try0.development@gmail.com>
2021-07-06 01:28:02 +0900

    Blazor Webアプリケーション

---
+++ src/StampImages.WebApp.Blazor/Models/StampModel.cs
@@ -0,0 +1,36 @@
+﻿using StampImages.Core;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StampImages.WebApp.Blazor.Models
+{
+    /// <summary>
+    /// スタンプモデル
+    /// </summary>
+    public class StampModel
+    {
+        /// <summary>
+        /// スタンプカラー
+        /// </summary>
+        public String ColorHexValue { get; set; } = ColorTranslator.ToHtml(BaseStamp.DEFAULT_STAMP_COLOR);
+
+        /// <summary>
+        /// スタンプカラー
+        /// </summary>
+        public Color Color => ColorTranslator.FromHtml(ColorHexValue);
+
+        /// <summary>
+        /// エッジタイプ
+        /// </summary>
+        public StampEdgeType EdgeType { get; set; } = StampEdgeType.SINGLE;
+
+        public StampModel()
+        {
+        }
+
+
+    }
+}
